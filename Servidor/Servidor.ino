e3f6dbe41a2b0f23535f2143072e09976578c8e2
Jonathan Oliveira <jojonathanjona87@gmail.com>
2020-09-28 00:19:25 -0300

    Comentários removidos

--- Servidor/Servidor.ino
+++ Servidor/Servidor.ino
@@ -14,7 +14,6 @@ void InitESPNow() {
   }
 }
 
-// config AP SSID
 void configDeviceAP() {
   const char *SSID = "servidor_1";
   bool result = WiFi.softAP(SSID, "servidor_1_Password", CHANNEL, 0);
@@ -41,7 +40,6 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   char macStr[18];
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print("Sinal recebido recebido de: "); Serial.println(macStr);
   Serial.print("Ultimo sinal recebido: "); Serial.println(*data);
   Serial.println("");
 }
